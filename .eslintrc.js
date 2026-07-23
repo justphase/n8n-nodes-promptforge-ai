fb5cdf077cbb97c5e40ce5db09b4c5ac5d978b50
Aaditya Mishra <justphase11@gmail.com>
2026-07-24 01:49:10 +0530

    Bump version to 0.1.4

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,8 +18,10 @@ module.exports = {
 			plugins: ['eslint-plugin-n8n-nodes-base'],
 			extends: ['plugin:n8n-nodes-base/nodes'],
 			rules: {
-				'n8n-nodes-base/node-param-fixed-collection-type-unsorted-items': 'off',
-			},
+                'n8n-nodes-base/node-param-fixed-collection-type-unsorted-items': 'off',
+                'n8n-nodes-base/node-class-description-inputs-wrong-regular-node': 'off',
+                'n8n-nodes-base/node-class-description-outputs-wrong': 'off',
+ 	        },
 		},
 	],
 };
